244fb66aa8d5647c5052a0ebb36e5bcfc756eaa4
Bigborg <100381400+EnzoBDF@users.noreply.github.com>
2022-03-17 12:33:29 -0300

    Add files via upload

---
+++ Questao_1_e_2.js
@@ -0,0 +1,23 @@
+
+var nome1 = "Enzo";
+var nome2 = "Bernardo";
+var nome3 = "Maria Eduarda";
+var nome4 = "Jaíza";
+var nome5 = "Cairé";
+
+ x = 12;
+ y = 45;
+ z = -86;
+ e = 9;
+ r = 34;
+
+
+console.log(x, y, z, e, r)
+console.log(nome1, nome2, nome3, nome4, nome5)
+
+
+
+
+
+
+
