3a854c1beb583a719f5e06b5b37271b777623346
Venom Infinity <99411987+BeMachado23@users.noreply.github.com>
2022-03-17 12:38:01 -0300

    Add files via upload

---
+++ Questao_3_e_4.js
@@ -0,0 +1,41 @@
+
+var nome1 = "Enzo"
+var nome2 = "Vênus"
+var nome3 = "Jaíza"
+var nome4 = "Cairé"
+var nome5 = "Babu Gavassi"
+var nome6 = "Bernardo"
+
+var cancelou = true
+var reprovou = false
+var humilhou = false
+var gabaritou = true
+var lacrou = true
+var mitou = true
+
+console.log(nome1, lacrou)
+console.log(nome2, lacrou)
+console.log(nome5, reprovou)
+console.log(nome4, mitou)
+console.log(nome3, gabaritou)
+console.log(nome6, humilhou)
+
+//
+
+funcionário1 = "Jonas"
+funcionário2 = "Alcino"
+funcionário3 = "Roberto"
+funcionário4 = "Gerivaldo"
+funcionário5 = "Antônio"
+
+salario1 = 2500.00
+salario2 = 1500.00
+salario3 = 5415.00
+salario4 = 10000.00
+salario5 = 3750.00
+
+console.log(funcionário1, salario1)
+console.log(funcionário2, salario2)
+console.log(funcionário3, salario3)
+console.log(funcionário4, salario4)
+console.log(funcionário5, salario5)
